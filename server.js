e79280f0ff1ae00aafd86a804010cde915784c2a
db46rt00ors <97573825@qq.com>
2019-11-06 13:59:58 +0800

    add id

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var formidable = require('formidable')
+var path = require('path');
 var fs = require('fs');
 var app = express();
 app.get('/getmusic', (req, res) => {
@@ -8,14 +9,47 @@ app.get('/getmusic', (req, res) => {
         res.json(arr)
     })
 });
-app.post('/getmusic',(req, res)=>{
+app.post('/uploadfiles', (req, res) => {
     var form = new formidable.IncomingForm();
- 
-    form.parse(req, function(err, fields, files) {
+    form.uploadDir = path.resolve(__dirname, './www/temps')
+    // 保留拓展名
+    form.keepExtensions = true
+
+    form.parse(req, function (err, fields, files) { 
+        var oldpath = files.file.path;
+        var newpath = path.join(path.dirname(oldpath), files.file.name);
+        fs.renameSync(
+            // 先读取文件
+            oldpath,
+            newpath
+        )
+        if(files.file.type == 'image/jpeg' || 'image/jpe' || 'image/png' || 'image/gif'){
+            res.send(path.parse(files.file.path).base);
+        }else{
+            res.send({ ok: '文件上传并改名成功！' });
+        }
+        
+    });
+})
+
+app.post('/addmusic', (req, res) => {
+    var form = new formidable.IncomingForm();
+   
+
+    form.parse(req, function (err, fields, files) { 
+        res.json({fields})
+    });
+})
+
+
+
+app.post('/getmusic', (req, res) => {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
         var type = fields.type
         fs.readFile('./data.json', (err, data) => {
             var arr = JSON.parse(data.toString()).mapList[type];
-            console.log(arr);
             res.json(arr)
         })
     })
