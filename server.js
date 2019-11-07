fd9f30bac1c807412392db3f815afd64587728f5
db46rt00ors <97573825@qq.com>
2019-11-07 10:31:40 +0800

    change

--- server.js
+++ server.js
@@ -16,28 +16,80 @@ app.post('/uploadfiles', (req, res) => {
     form.keepExtensions = true
 
     form.parse(req, function (err, fields, files) { 
-        var oldpath = files.file.path;
-        var newpath = path.join(path.dirname(oldpath), files.file.name);
-        fs.renameSync(
-            // 先读取文件
-            oldpath,
-            newpath
-        )
-        if(files.file.type == 'image/jpeg' || 'image/jpe' || 'image/png' || 'image/gif'){
-            res.send(path.parse(files.file.path).base);
-        }else{
-            res.send({ ok: '文件上传并改名成功！' });
-        }
+        // var oldpath = files.file.path;
+        // var newpath = path.join(path.dirname(oldpath), files.file.name);
+        // fs.renameSync(
+        //     // 先读取文件
+        //     oldpath,
+        //     newpath
+        // )
+
+        res.send(path.parse(files.file.path).base);
+
+        // if(files.file.type == 'image/jpeg' || 'image/jpe' || 'image/png' || 'image/gif'){
+        //     res.send(path.parse(files.file.path).base);
+        // }else{
+        //     res.send(path.parse(files.file.path).base);
+        // }
         
     });
 })
 
 app.post('/addmusic', (req, res) => {
+    var id = '';
+    var str = '123456789qwertyuioplkjhgfdsazxcvbbnmm';
+    for(let i = 0; i< 10; i++){
+        id += str[~~(Math.random() * str.length)]
+    }
     var form = new formidable.IncomingForm();
-   
-
     form.parse(req, function (err, fields, files) { 
-        res.json({fields})
+        fs.mkdirSync(path.resolve(__dirname, './www/music/recommend/'+ id));
+
+        let musicName  = fields.form.musicName
+        let musicSinger = fields.form.musicSinger
+        let musicUrl = fields.form.musicUrl
+        let musicLrc = fields.form.musicLrc
+        let singerImg = fields.form.singerImg
+        let playerbg = fields.form.playerbg
+        fs.readFile('./data.json', (err, data) => {
+            var {mapList} = JSON.parse(data.toString());
+            var arr = mapList.recommend
+            var musicinfo = {
+                id,
+                musicName,
+                musicSinger,
+                musicUrl: `/music/recommend/${id}/${musicUrl}`,
+                musicLrc: `/music/recommend/${id}/${musicLrc}`,
+                singerImg: `/music/recommend/${id}/${singerImg}`,
+                playerbg: `/music/recommend/${id}/${playerbg}`
+            }
+            arr.unshift(musicinfo);
+            var obj = {
+                ...mapList,
+                recommend: arr
+            }
+            console.log(obj)
+            fs.renameSync(
+                path.resolve(__dirname, `./www/temps/${musicUrl}`),
+                path.resolve(__dirname, `./www/music/recommend/${id}/${musicUrl}`)
+            );
+            fs.renameSync(
+                path.resolve(__dirname, `./www/temps/${musicLrc}`),
+                path.resolve(__dirname, `./www/music/recommend/${id}/${musicLrc}`)
+            );
+            fs.renameSync(
+                path.resolve(__dirname, `./www/temps/${singerImg}`),
+                path.resolve(__dirname, `./www/music/recommend/${id}/${singerImg}`)
+            );
+            fs.renameSync(
+                path.resolve(__dirname, `./www/temps/${playerbg}`),
+                path.resolve(__dirname, `./www/music/recommend/${id}/${playerbg}`)
+            );
+            fs.writeFile('./data.json',JSON.stringify({mapList: obj}),(err) => {
+                res.json(obj)
+            })
+ 
+        })
     });
 })
 
